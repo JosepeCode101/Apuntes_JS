c7cd3b8270201f4831d1c5989f37ea8eb582713b
JosepeCode <72314772+JosepeCode101@users.noreply.github.com>
2022-11-23 16:34:20 +0100

    breaking down the idea I have in mind

--- Recursive_Functions/creating_streams_with_rf/js/app.js
+++ Recursive_Functions/creating_streams_with_rf/js/app.js
@@ -1,9 +1,27 @@
 "use strict"
 
+// 1 - Elaborar un formulario html
+// 1.1 - Asignar ids para capturar los datos que se recogen del formulario
+// 2 - Elaborar una función recursiva que verifique que todos los campos 
+//     del formulario están correctamente introducidos
+// 3 - Crear una función para generar un txt (modelo correo electrónico) a 
+//     través de streams y los datos introducidos en el formulario
+// 3.1 - 
+// 4 - Elaborar un botón (div) para finalizar el proceso
+// 4.1 - Que ejecute la funcion recursiva 
+// 4.2 - Que ejecute la funcion de emitir el txt
+
+
+
+
+
+
 // probando código antiguo
 
+/*
+
 const fs = require('fs');
-const readline = require('readline');
+const readline = require('readline');D
 
 let i = readline.createInterface(process.stdin, process.stdout);
 
@@ -21,3 +39,5 @@ i.question('Cual es tu nombre?', (nombre)=>{
     });
 
 });
+
+*/
