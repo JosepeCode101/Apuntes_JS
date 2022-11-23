d4d6e4207721399ee261b369321608c850d87317
JosepeCode <72314772+JosepeCode101@users.noreply.github.com>
2022-11-23 12:27:29 +0100

    Robustly asking data with easy prompts

--- Recursive_Functions/asking_data_with_recursive_functions/js/app.js
+++ Recursive_Functions/asking_data_with_recursive_functions/js/app.js
@@ -1,1 +1,38 @@
 "use strict"
+//console.log('Esto se está ejecutando');
+function getName() {
+    const name = prompt('Inserte un nombre');
+    return isNaN(name) ? name : getName();
+}
+
+const result = getName();
+//console.log(result);
+
+function getAge () {
+    const age = prompt('Inserte su edad')
+    return !isNaN(age) ? age:getAge();
+}
+
+const result2 = getAge();
+//console.log(result2);
+
+
+function getNumericPrompt(message) {
+    const age = prompt(message);
+    return !isNaN(age) ?  age : getNumericPrompt(message);
+}
+  
+function getStringPrompt(message) {
+    const name = prompt(message);
+    return isNaN(name) ?  name : getStringPrompt(message);
+}
+  
+function getPerson() {
+    const name = getStringPrompt('Dime tu nombre');
+    const age = getNumericPrompt('Dime tu edad');
+   return { name, age };
+}
+  
+const {name, age} = getPerson();
+//console.log('Esto se está ejecutando');
+//alert('Hola' + {name} + 'con' + {age} + 'años');
