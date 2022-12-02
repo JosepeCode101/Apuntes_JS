8cbe15f9d78b400bb33a2fc00c8444108d63c698
JosepeCode <72314772+JosepeCode101@users.noreply.github.com>
2022-12-02 12:00:38 +0100

    Starting with events

--- working_with_events/js/app.js
+++ working_with_events/js/app.js
@@ -1,1 +1,17 @@
 "use strict"
+
+// Mouse Events
+
+// Asociar elementos del html con el JS para trabar con ellos
+const boton = document.querySelector('.boton');
+
+// Escucha de eventos que van a ir sucediendo con el 
+// click
+boton.addEventListener('click', function(){
+    console.log("The button has been clicked")
+})
+
+//mouseover
+boton.addEventListener('mouseover', function(){
+    console.log("The mouse is over the button");
+})
