ea63c4fbbcb789133f28752d24d63c2070d05f67
JosepeCode <72314772+JosepeCode101@users.noreply.github.com>
2022-12-08 11:28:05 +0100

    Keyboard and load events

--- working_with_events/js/app.js
+++ working_with_events/js/app.js
@@ -8,7 +8,10 @@ const boton = document.querySelector('.boton');
 // Escucha de eventos que van a ir sucediendo con el 
 // click
 boton.addEventListener('click', function(){
-    console.log("The button has been clicked")
+    console.log("The button has been clicked");
+    //while (true) {
+    //console.log("V A    T Í O")
+    //}
 })
 
 //mouseover
@@ -19,4 +22,25 @@ boton.addEventListener('mouseover', function(){
 // mouseout
 boton.addEventListener('mouseout', function(){
     console.log("The mouse is out the button");
+})
+
+// Keyboard events 
+// Keydown
+window.addEventListener("keydown", function(event){
+    console.log('The key was DOWN pressed');
+})
+// Keypress
+window.addEventListener("keypress", function(event){
+    console.log('The key was pressed');
+})
+// KeyUp
+window.addEventListener("keyup", function(event){
+    console.log('The key is up');
+    console.log(String.fromCharCode(event.keyCode));
+})
+
+// Evento de Carga, Escuchar cuando se ha cargado el documento para ejecutar una acción 
+
+window.addEventListener('load', function(){
+    console.log('Se ha cargado el documento');
 })
